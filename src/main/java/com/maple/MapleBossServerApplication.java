09277957d783e6aa6248d5fccc58f7aa06217b99
Woogie Kim <mdir2kor@gmail.com>
2022-04-09 09:03:30 +0900

    first initialize

---
+++ src/main/java/com/maple/MapleBossServerApplication.java
@@ -0,0 +1,13 @@
+package com.maple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleBossServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleBossServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maple/MapleBossServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleBossServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
