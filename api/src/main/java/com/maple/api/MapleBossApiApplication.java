984450347bd0ac9b6689ff67c7bef2081f9dad6f
Woogie Kim <mdir2kor@gmail.com>
2022-04-15 20:26:04 +0900

    MAPLE-13 메인 실행시 빠진 args 추가

--- api/src/main/java/com/maple/api/MapleBossApiApplication.java
+++ api/src/main/java/com/maple/api/MapleBossApiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MapleBossApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MapleBossApiApplication.class);
+        SpringApplication.run(MapleBossApiApplication.class, args);
     }
 }
