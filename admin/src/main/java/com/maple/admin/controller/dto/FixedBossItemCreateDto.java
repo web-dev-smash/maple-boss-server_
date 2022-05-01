ad6b4e29ab5d9003c22e4a7ac880a928bde2bda9
Woogie Kim <mdir2kor@gmail.com>
2022-05-01 13:22:50 +0900

    MAPLE-29 어드민 BossItem API 개발

---
+++ admin/src/main/java/com/maple/admin/controller/dto/FixedBossItemCreateDto.java
@@ -0,0 +1,37 @@
+package com.maple.admin.controller.dto;
+
+import com.maple.common.bossitem.domain.FixedBossItem;
+import com.maple.common.bossitem.domain.FixedBossItemAmount;
+import lombok.val;
+
+public class FixedBossItemCreateDto {
+
+    public record FixedBossItemCreateRequest(
+            long bossId,
+            long itemId,
+            int minimumAmount,
+            int maximumAmount,
+            Long price
+    ) {
+
+        public com.maple.admin.service.dto.FixedBossItemCreateDto toDto() {
+            return new com.maple.admin.service.dto.FixedBossItemCreateDto(bossId, itemId, new FixedBossItemAmount(minimumAmount, maximumAmount), price);
+        }
+    }
+
+    public record FixedBossItemCreateResponse(FixedBossItemCreateData fixedBossItem) {
+    }
+
+    public record FixedBossItemCreateData(
+            long id,
+            int minimumAmount,
+            int maximumAmount,
+            long price
+    ) {
+        public static FixedBossItemCreateData create(FixedBossItem fixedBossItem) {
+            val amount = fixedBossItem.getAmount();
+
+            return new FixedBossItemCreateData(fixedBossItem.getId(), amount.getMinimum(), amount.getMaximum(), fixedBossItem.getPrice());
+        }
+    }
+}
---
+++ admin/src/main/java/com/maple/admin/controller/dto/RandomBossItemCreateDto.java
@@ -0,0 +1,35 @@
+package com.maple.admin.controller.dto;
+
+import com.maple.common.bossitem.domain.RandomBossItem;
+import com.maple.common.bossitem.domain.RandomBossItemAmount;
+import lombok.val;
+
+public class RandomBossItemCreateDto {
+
+    public record RandomBossItemCreateRequest(
+            long bossId,
+            long itemId,
+            int minimumAmount,
+            int maximumAmount
+    ) {
+
+        public com.maple.admin.service.dto.RandomBossItemCreateDto toDto() {
+            return new com.maple.admin.service.dto.RandomBossItemCreateDto(bossId, itemId, new RandomBossItemAmount(minimumAmount, maximumAmount));
+        }
+    }
+
+    public record RandomBossItemCreateResponse(RandomBossItemCreateData fixedBossItem) {
+    }
+
+    public record RandomBossItemCreateData(
+            long id,
+            int minimumAmount,
+            int maximumAmount
+    ) {
+        public static RandomBossItemCreateData create(RandomBossItem randomBossItem) {
+            val amount = randomBossItem.getAmount();
+
+            return new RandomBossItemCreateData(randomBossItem.getId(), amount.getMinimum(), amount.getMaximum());
+        }
+    }
+}
