541b22a173d75d0724fa355ec561cb980cad1e95
peubel <sksqkrwns@naver.com>
2022-05-07 16:02:29 +0900

    MAPLE-47 Party 도메인 개발 작업중

--- common/src/main/java/com/maple/common/party/domain/Party.java
+++ common/src/main/java/com/maple/common/party/domain/Party.java
@@ -1,14 +1,18 @@
 package com.maple.common.party.domain;
 
+import com.google.common.base.Preconditions;
 import com.maple.common.support.BaseEntity;
 import com.maple.common.user.domain.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 
 import javax.persistence.*;
 import java.time.OffsetDateTime;
 import java.util.List;
+
+import static com.google.common.base.Preconditions.*;
 
 /**
  * 파티
@@ -18,14 +22,6 @@ import java.util.List;
 @Entity
 public class Party extends BaseEntity {
 
-    /* 이름 */
-    @Column(length = 15, nullable = false)
-    private String name;
-
-    /* 부가설명 */
-    @Column(length = 50)
-    private String description;
-
     /* 파티장 */
     @ManyToOne(fetch = FetchType.LAZY)
     private User leader;
@@ -33,6 +29,14 @@ public class Party extends BaseEntity {
     /* 파티원 */
     @ManyToMany
     private List<User> members;
+
+    /* 이름 */
+    @Column(length = 15, nullable = false)
+    private String name;
+
+    /* 부가설명 */
+    @Column(length = 50)
+    private String description;
 
     /* 상태 */
     @Enumerated(EnumType.STRING)
@@ -42,10 +46,23 @@ public class Party extends BaseEntity {
     /* 생성일 */
     private OffsetDateTime createAt = OffsetDateTime.now();
 
-    public Party(String name, String description, User leader, List<User> members) {
+    public Party(User leader, String name, String description) {
+        checkNotNull(leader);
+
+        checkArgument(Strings.isNotBlank(name));
+
+        this.leader = leader;
         this.name = name;
         this.description = description;
-        this.leader = leader;
-        this.members = members;
+    }
+
+    public Party withMember(User member) {
+//        if (members.contains(member)) {
+//            throw new MapleBossException(ALREADY_EXISTS_MEMBER);
+//        }
+
+        this.members.add(member);
+
+        return this;
     }
 }
--- common/src/test/java/com/maple/common/party/domain/PartyTest.java
+++ common/src/test/java/com/maple/common/party/domain/PartyTest.java
@@ -1,14 +1,18 @@
 package com.maple.common.party.domain;
 
+import com.maple.common.boss.domain.Boss;
+import com.maple.common.boss.domain.BossClass;
 import com.maple.common.fixture.UserFixture;
 import com.maple.common.user.domain.User;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class PartyTest {
 
@@ -29,13 +33,36 @@ class PartyTest {
 
     @Test
     void 파티_생성_성공() {
-        val party = new Party("파티", "파티부가설명", leader, List.of(user1, user2, user3));
+        val party = new Party(leader, "파티", "파티부가설명");
 
         assertThat(party.getName()).isEqualTo("파티");
         assertThat(party.getDescription()).isEqualTo("파티부가설명");
         assertThat(party.getLeader()).isEqualTo(leader);
-        assertThat(party.getMembers()).containsExactly(user1, user2, user3);
+        assertThat(party.getMembers()).isNull();
         assertThat(party.getStatus()).isEqualTo(PartyStatus.CREATED);
         assertThat(party.getCreateAt()).isNotNull();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 파티_이름이_널이거나_빈값이면_실패(String name) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Party(leader, name, "파티부가설명"));
+    }
+
+    @Test
+    void 파티_이름이_공백이면_실패() {
+        val emptyPartyName = "  ";
+
+        assertThatIllegalArgumentException().isThrownBy(() -> new Party(leader, emptyPartyName, "파티부가설명"));
+    }
+
+    @Test
+    void 파티장이_널이면_실패() {
+        assertThatNullPointerException().isThrownBy(() -> new Party(null, "파티", "파티부가설명"));
+    }
+
+    @Test
+    void 파티원_추가_성공() {
+
     }
 }
