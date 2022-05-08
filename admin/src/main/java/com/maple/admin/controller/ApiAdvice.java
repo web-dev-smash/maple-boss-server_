7ce57007a02110f650416a651685ff0fa76fa872
Woogie Kim <mdir2kor@gmail.com>
2022-05-08 21:32:32 +0900

    MAPLE-57 공통 예외 프로파일 지정

--- admin/src/main/java/com/maple/admin/controller/ApiAdvice.java
+++ admin/src/main/java/com/maple/admin/controller/ApiAdvice.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Profile({"dev", "prod"})
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -22,24 +24,6 @@ public class ApiAdvice {
     public static final String LOG_CHANNEL = "C03BKF0CYL8";
     public static final String BOT_NAME = "Maple Boss Admin";
 
-    @ExceptionHandler(MapleBossException.class)
-    public ErrorResponse mapleBossException(MapleBossException ex) {
-        log.error(ex.getMessage(), ex);
-
-        val message = messageSource.getMessage(ex.getMessage(), null, LocaleContextHolder.getLocale());
-
-        return new ErrorResponse(new ErrorData(message));
-    }
-
-    @ExceptionHandler(RuntimeException.class)
-    public ErrorResponse runtimeException(RuntimeException ex) {
-        log.error(ex.getMessage(), ex);
-
-        eventPublisher.publishEvent(new UnhandledExceptionEvent(BOT_NAME, LOG_CHANNEL, ex));
-
-        return new ErrorResponse(new ErrorData("예상치 못한 에러가 발생했습니다."));
-    }
-
     @ExceptionHandler(Exception.class)
     public ErrorResponse exception(Exception ex) {
         log.error(ex.getMessage(), ex);
@@ -47,6 +31,15 @@ public class ApiAdvice {
         eventPublisher.publishEvent(new UnhandledExceptionEvent(BOT_NAME, LOG_CHANNEL, ex));
 
         return new ErrorResponse(new ErrorData("예상치 못한 에러가 발생했습니다."));
+    }
+
+    @ExceptionHandler(MapleBossException.class)
+    public ErrorResponse mapleBossException(MapleBossException ex) {
+        log.error(ex.getMessage(), ex);
+
+        val message = messageSource.getMessage(ex.getMessage(), null, LocaleContextHolder.getLocale());
+
+        return new ErrorResponse(new ErrorData(message));
     }
 
     public record ErrorResponse(ErrorData error) {
