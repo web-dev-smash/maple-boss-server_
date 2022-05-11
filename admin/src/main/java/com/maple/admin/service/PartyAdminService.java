7f5cf828a44e0bbf616948d22ead44255c3c1d39
peubel <sksqkrwns@naver.com>
2022-05-11 20:58:42 +0900

    MAPLE-52 어드민 Party 전체 목록 조회 서비스 개발

---
+++ admin/src/main/java/com/maple/admin/service/PartyAdminService.java
@@ -0,0 +1,13 @@
+package com.maple.admin.service;
+
+import com.maple.common.party.domain.Party;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface PartyAdminService {
+    
+    /**
+     * 전체 파티 목록 조회
+     */
+    Page<Party> getAllParties(Pageable pageable);
+}
