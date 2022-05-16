c6d151d574b2f86b6d6b90707ca01cf4d1d8b6cc
peubel <sksqkrwns@naver.com>
2022-05-16 13:21:02 +0900

    MAPLE-64 DomainPartyService 내용 수정
    * UserRepository 필드 삭제

--- common/src/main/java/com/maple/common/party/service/DomainPartyService.java
+++ common/src/main/java/com/maple/common/party/service/DomainPartyService.java
@@ -3,7 +3,6 @@ package com.maple.common.party.service;
 import com.maple.common.party.domain.Party;
 import com.maple.common.party.domain.PartyRepository;
 import com.maple.common.user.domain.User;
-import com.maple.common.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +17,6 @@ import static com.maple.core.exception.Preconditions.notNull;
 public class DomainPartyService implements PartyService {
 
     private final PartyRepository partyRepository;
-    private final UserRepository userRepository;
 
     @Override
     public Party create(Party party) {
