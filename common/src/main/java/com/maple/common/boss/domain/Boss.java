7d9efa02c449c7557f09f16087b8ee8cfdf5b453
peubel <sksqkrwns@naver.com>
2022-05-16 20:34:17 +0900

    MAPLE-64 List import 수정

--- common/src/main/java/com/maple/common/boss/domain/Boss.java
+++ common/src/main/java/com/maple/common/boss/domain/Boss.java
@@ -7,11 +7,11 @@ import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 
 import javax.persistence.*;
+import java.util.List;
 
 import static com.maple.core.exception.Preconditions.require;
 import static java.util.Collections.max;
 import static java.util.Collections.min;
-import static java.util.List.of;
 
 /**
  * 보스
@@ -70,11 +70,11 @@ public class Boss extends BaseEntity {
                 final Long hpPhaseOne, final Long hpPhaseTwo, final Long hpPhaseThree, final Long hpPhaseFour,
                 final int arcaneForce, final int deathLimit) {
         require(Strings.isNotBlank(name), "이름은 필수입니다.");
-        require(max(of(entryMinLevel, entryMaxLevel)) <= MAX_LEVEL, "최대 입장 가능 레벨을 초과 할 수 없습니다.");
-        require(min(of(entryMinLevel, entryMaxLevel)) >= MIN_LEVEL, "최소 입장 가능 레벨 미만일 수 없습니다.");
+        require(max(List.of(entryMinLevel, entryMaxLevel)) <= MAX_LEVEL, "최대 입장 가능 레벨을 초과 할 수 없습니다.");
+        require(min(List.of(entryMinLevel, entryMaxLevel)) >= MIN_LEVEL, "최소 입장 가능 레벨 미만일 수 없습니다.");
         require(entryMinLevel < entryMaxLevel, "입장 최소는 최대보다 작아야 합니다.");
-        require(min(of(level, entryMinLevel, entryMaxLevel, arcaneForce, deathLimit)) > -1, "음수는 올 수 없습니다.");
-        require(min(of(hpPhaseOne, hpPhaseTwo, hpPhaseThree, hpPhaseFour)) > -1, "음수는 올 수 없습니다.");
+        require(min(List.of(level, entryMinLevel, entryMaxLevel, arcaneForce, deathLimit)) > -1, "음수는 올 수 없습니다.");
+        require(min(List.of(hpPhaseOne, hpPhaseTwo, hpPhaseThree, hpPhaseFour)) > -1, "음수는 올 수 없습니다.");
 
         this.name = name;
         this.level = level;
--- common/src/main/java/com/maple/common/bossitem/domain/FixedBossItemAmount.java
+++ common/src/main/java/com/maple/common/bossitem/domain/FixedBossItemAmount.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import java.util.List;
 
 import static com.maple.core.exception.Preconditions.require;
 import static java.util.Collections.min;
-import static java.util.List.of;
 
 /**
  * 보스 아이템 수량
@@ -28,7 +28,7 @@ public class FixedBossItemAmount {
     private int maximum;
 
     public FixedBossItemAmount(int minimum, int maximum) {
-        require(min(of(minimum, maximum)) >= ALLOWED_MINIMUM_AMOUNT, "최소 수량과 최대 수량은 최소 {0}개 이상이여야 합니다.", ALLOWED_MINIMUM_AMOUNT);
+        require(min(List.of(minimum, maximum)) >= ALLOWED_MINIMUM_AMOUNT, "최소 수량과 최대 수량은 최소 {0}개 이상이여야 합니다.", ALLOWED_MINIMUM_AMOUNT);
         require(minimum <= maximum, "최대 수량이 최소 수량 보다 적을 수 없습니다.");
 
         this.minimum = minimum;
