6b1e1adcaff27237095f43859eced1b3b680269b
peubel <sksqkrwns@naver.com>
2022-05-18 00:54:48 +0900

    MAPLE-45 어드민 User 목록 조회 서비스 개발

---
+++ admin/src/test/java/com/maple/admin/service/DefaultUserAdminServiceTest.java
@@ -0,0 +1,51 @@
+package com.maple.admin.service;
+
+import com.maple.common.user.domain.CertCodeGenerator;
+import com.maple.common.user.domain.User;
+import com.maple.common.user.service.UserService;
+import com.maple.core.support.BaseServiceTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DefaultUserAdminServiceTest extends BaseServiceTest {
+
+    @Autowired
+    private UserAdminService userAdminService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CertCodeGenerator mockCertCodeGenerator;
+
+    private User user1;
+    private User user2;
+    private User user3;
+    private User user4;
+    private User user5;
+    private User user6;
+
+    @BeforeEach
+    void setUp() {
+        user1 = userService.create(new User("id1", "password1", "nick1", "email1"), mockCertCodeGenerator);
+        user2 = userService.create(new User("id2", "password2", "nick2", "email2"), mockCertCodeGenerator);
+        user3 = userService.create(new User("id3", "password3", "nick3", "email3"), mockCertCodeGenerator);
+        user4 = userService.create(new User("id4", "password4", "nick4", "email4"), mockCertCodeGenerator);
+        user5 = userService.create(new User("id5", "password5", "nick5", "email5"), mockCertCodeGenerator);
+        user6 = userService.create(new User("id6", "password6", "nick6", "email6"), mockCertCodeGenerator);
+    }
+
+    @Test
+    void 전체_유저_조회_성공() {
+        Page<User> page1Users = userAdminService.getAllUser(PageRequest.of(0, 4));
+        Page<User> page2Users = userAdminService.getAllUser(PageRequest.of(1, 4));
+
+        assertThat(page1Users).containsExactly(user1, user2, user3, user4);
+        assertThat(page2Users).containsExactly(user5, user6);
+    }
+}
