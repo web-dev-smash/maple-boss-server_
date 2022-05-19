ae18aa5e5fbcdb292fa514b424ecec9fa5c6fa18
goyounha11 <goyounha11@naver.com>
2022-05-19 14:39:44 +0900

    MAPLE-72 유저 비활성화 API 개발

---
+++ api/src/main/java/com/maple/api/controller/dto/UserPrepareWithdrawalDto.java
@@ -0,0 +1,18 @@
+package com.maple.api.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class UserPrepareWithdrawalDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class UserPrepareWithdrawalRequest {
+        private long id;
+        private String certCode;
+
+        public com.maple.api.service.dto.UserPrepareWithdrawalDto toDto(){
+            return new com.maple.api.service.dto.UserPrepareWithdrawalDto(this.id, this.certCode);
+        }
+    }
+}
